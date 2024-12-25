6f9428c5bf57222808cc3d705bd8b01fc4e75cb9
saussky <patrickgardnerbrunton@hotmail.com>
2024-12-25 11:21:39 +1100

    Fixing different sized images

--- src/components/productCard.tsx
+++ src/components/productCard.tsx
@@ -26,7 +26,7 @@ export default function ProductCard({
   handle,
   skateShop,
 }: ProductCardProps) {
-  const fallbackImageSrc = "/placeholder.jpg"; // Ensure the image is in `public/placeholder.jpg`
+  const fallbackImageSrc = "/placeholder.jpg";
   const productUrl = "hi"; // Replace with the correct logic for generating product URLs.
 
   return (
@@ -36,15 +36,17 @@ export default function ProductCard({
       rel="noopener noreferrer"
       className="block"
     >
-      <div className="border rounded-lg shadow-md p-4" key={id}>
+      <div className="border rounded-lg shadow-md p-4 h-full" key={id}>
         <p>{skateShop.name}</p>
-        <Image
-          src={imageSrc || fallbackImageSrc}
-          alt={title}
-          width={300}
-          height={300}
-          className="rounded"
-        />
+        <div className="relative aspect-[1/1]">
+          <Image
+            src={imageSrc || fallbackImageSrc}
+            alt={title}
+            layout="fill"
+            objectFit="cover"
+            className="rounded"
+          />
+        </div>
         <h2 className="mt-2 text-xl font-bold text-gray-800">{title}</h2>
         <p className="text-gray-600 mt-1">${price}</p>
       </div>
