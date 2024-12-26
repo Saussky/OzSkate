00daae8a474c1739034e44374ba3f66087adc2ab
saussky <patrickgardnerbrunton@hotmail.com>
2024-12-26 15:59:37 +1100

    Fixing todo

--- src/lib/product/transform.ts
+++ src/lib/product/transform.ts
@@ -6,7 +6,7 @@ import { transformVariants } from "./variants";
 // TODO: Varaints have sku's which SHOULD be global, use to identify and match products
 // TODO cont.: They aren't, but it some cases could be so it might be worth having that as a step but not the whole process
 // TODO: Implement Levenshtein distance to compare product names to find duplicates  https://en.wikipedia.org/wiki/Levenshtein_distance
-// TODO: master category taxonomy
+// TODO: Create master category taxonomy, documented
 
 export const transformProducts = (
   allPaginatedProducts: any[],
