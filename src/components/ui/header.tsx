81d3911d65689b460d1ae97dce96c9df8e5de8b7
saussky <patrickgardnerbrunton@hotmail.com>
2025-01-12 20:01:47 +1100

    Adding link to header

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -9,11 +9,11 @@ type HeaderProps = {
 export default function Header({ user }: HeaderProps) {
   return (
     <header className="flex justify-between items-center p-4 bg-gray-900 shadow-lg">
-      <div className="text-3xl font-bold">
+      <a href={'/'} className="text-3xl font-bold">
         <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 via-yellow-400 to-black">
           OzSkate
         </span>
-      </div>
+      </a>
 
       <div>
         <LoginBox initialUser={user} />
