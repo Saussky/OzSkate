d69622b7fe9e4aabdc370930309cfe8addf35b7f
saussky <patrickgardnerbrunton@hotmail.com>
2025-01-12 20:00:27 +1100

    Protecting admin page

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -1,15 +1,21 @@
 'use server';
 import { getProductCount, getShopCount, getShopNames } from '@/lib/actions';
 import AdminComponent from '@/components/admin';
+import { validateRequest } from '@/lib/lucia';
 
 export default async function Admin() {
+  const { user } = await validateRequest();
+
+  if (!user?.admin) {
+    return <small>Long live this thing</small>;
+  }
+
   const productCount = await getProductCount();
   const shopCount = await getShopCount();
   const shopNames = await getShopNames();
 
   return (
     <div>
-      <small>Long live this thing</small>
       <AdminComponent
         shopCount={shopCount}
         productCount={productCount}
