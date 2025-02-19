9ab7f1470d7646fec4db21758dec805caf08a3bb
saussky <patrickgardnerbrunton@hotmail.com>
2025-02-19 21:22:20 +1100

    uncommenting out admin flag

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -6,10 +6,9 @@ import { validateRequest } from '@/lib/lucia';
 export default async function Admin() {
   const { user } = await validateRequest();
 
-  //TODO: Uncomment
-  // if (!user?.admin) {
-  //   return <small>Long live this thing</small>;
-  // }
+  if (!user?.admin) {
+    return <small>Long live this thing</small>;
+  }
 
   const productCount = await getProductCount();
   const shopCount = await getShopCount();
