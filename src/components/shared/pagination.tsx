9483e4747781a0783f8ef8b72c127175b908934c
saussky <patrickgardnerbrunton@hotmail.com>
2025-03-10 12:49:12 +1100

    Reformatting pagination

--- src/components/shared/pagination.tsx
+++ src/components/shared/pagination.tsx
@@ -1,3 +1,7 @@
+'use client';
+import React from 'react';
+import Button from '../ui/button';
+
 interface PaginationProps {
   currentPage: number;
   totalPages: number;
@@ -8,7 +12,7 @@ export default function Pagination({
   currentPage,
   totalPages,
   onPageChange,
-}: PaginationProps) {
+}: PaginationProps): JSX.Element {
   const handleNextPage = () => {
     if (currentPage < totalPages) {
       onPageChange(currentPage + 1);
@@ -22,26 +26,22 @@ export default function Pagination({
   };
 
   return (
-    <div className="flex justify-between items-center mt-4 w-72">
-      <button
-        onClick={handlePrevPage}
-        disabled={currentPage === 1}
-        className="bg-gray-300 px-4 py-2 rounded disabled:opacity-50"
-      >
+    <div className="flex items-center gap-2">
+      <Button onClick={handlePrevPage} variant="smart">
         Previous
-      </button>
+      </Button>
 
-      <span className="text-gray-600">
+      <span className="text-sm text-black">
         Page <strong>{currentPage}</strong> of <strong>{totalPages}</strong>
       </span>
 
-      <button
+      <Button
         onClick={handleNextPage}
         disabled={currentPage === totalPages}
-        className="bg-blue-500 text-white px-4 py-2 rounded disabled:opacity-50"
+        variant="smart"
       >
         Next
-      </button>
+      </Button>
     </div>
   );
 }
