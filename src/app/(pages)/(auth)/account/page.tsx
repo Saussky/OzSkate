5273d950cd6841bb318e4c6e3aaf0eaa4b0c4633
saussky <patrickgardnerbrunton@hotmail.com>
2025-03-16 18:18:46 +1100

    Fixing account page slightly

--- src/app/(pages)/(auth)/account/page.tsx
+++ src/app/(pages)/(auth)/account/page.tsx
@@ -1,6 +1,6 @@
 'use server';
 import Account from '@/components/auth/account/account';
 
-export default function AccountPage() {
+export default async function AccountPage() {
   <Account />;
 }
--- src/components/admin/duplicates/products/actions.ts
+++ src/components/admin/duplicates/products/actions.ts
@@ -1,5 +1,5 @@
-'use server';
 /* eslint-disable @typescript-eslint/no-explicit-any */
+'use server';
 import { prisma } from '@/lib/prisma';
 import { checkProductSimilarity } from '@/lib/product/merge';
 import { ProductWithSuspectedDuplicate } from '@/lib/types';
