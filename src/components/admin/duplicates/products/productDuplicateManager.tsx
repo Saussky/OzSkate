e9bba185016f8a6c7dc3f7261e2f4f45d26873bf
saussky <patrickgardnerbrunton@hotmail.com>
2025-03-30 11:54:43 +1100

    re-enabling

--- src/components/admin/duplicates/products/productDuplicateManager.tsx
+++ src/components/admin/duplicates/products/productDuplicateManager.tsx
@@ -9,18 +9,18 @@ import {
 } from './actions';
 import ProductCard from '@/components/shared/product-card/productCard';
 import Pagination from '@/components/shared/pagination';
+import { Prisma } from '@prisma/client';
 
-// type DuplicatePair = Prisma.ProductDuplicateGetPayload<{
-//   include: {
-//     masterProduct: { include: { shop: true } };
-//     duplicateProduct: { include: { shop: true } };
-//   };
-// }>;
+type DuplicatePair = Prisma.ProductDuplicateGetPayload<{
+  include: {
+    masterProduct: { include: { shop: true } };
+    duplicateProduct: { include: { shop: true } };
+  };
+}>;
 
 export default function ProductDuplicateManager(): JSX.Element {
   const [isFinding, startFinding] = useTransition();
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [duplicates, setDuplicates] = useState<any[]>([]);
+  const [duplicates, setDuplicates] = useState<DuplicatePair[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(1);
   const [isPending, startTransition] = useTransition();
