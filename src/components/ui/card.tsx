20fb143afa569f2f469af05916f818ca18f0cb46
saussky <patrickgardnerbrunton@hotmail.com>
2025-06-25 20:22:41 +1000

    fixing log width

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 export default function Card({ title, children }: CardProps): JSX.Element {
   return (
-    <div className="flex-1 max-w-sm p-6 bg-white rounded-lg shadow-lg">
+    <div className="flex-1  p-6 bg-white rounded-lg shadow-lg">
       <p className="text-lg font-semibold text-gray-700">{title}</p>
       <div className="mt-2 text-3xl font-bold text-blue-500">{children}</div>
     </div>
