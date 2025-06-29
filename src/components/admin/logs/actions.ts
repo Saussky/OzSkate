97af99f3d59daa70f4debe39528bb5b6841a7e01
saussky <patrickgardnerbrunton@hotmail.com>
2025-06-29 19:08:13 +1000

    allowing anys

--- src/components/admin/logs/actions.ts
+++ src/components/admin/logs/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 import { prisma } from '@/lib/prisma';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function fetchLogs(): Promise<any[]> {
   // Fetch all logs, latest first
   const logs = await prisma.productUpdateLog.findMany({
