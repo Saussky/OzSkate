e6af0c1dfd8cd46bc0ab95f7a41f2fc383dcab3e
saussky <patrickgardnerbrunton@hotmail.com>
2025-09-13 21:34:35 +1000

    commenting out logs

--- src/app/(pages)/admin/logs/page.tsx
+++ src/app/(pages)/admin/logs/page.tsx
@@ -1,10 +1,9 @@
-// app/admin/logs/page.tsx
-import AdminLogsComponent from '@/components/admin/logs/logs';
-import { prisma } from '@/lib/prisma';
+//import AdminLogsComponent from '@/components/admin/logs/logs';
+// import { prisma } from '@/lib/prisma';
 
 export default async function LogsPage() {
-  const initialLogs = await prisma.productUpdateLog.findMany({
-    orderBy: { createdAt: 'desc' },
-  });
-  return <AdminLogsComponent initialLogs={initialLogs} />;
+  // const initialLogs = await prisma.productUpdateLog.findMany({
+  //   orderBy: { createdAt: 'desc' },
+  // });
+  return <div>hi</div>;
 }
